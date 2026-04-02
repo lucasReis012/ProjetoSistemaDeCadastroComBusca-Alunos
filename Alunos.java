db57dce8bcda1b437460bbba541860142b145f64
lucasReis012 <129999865+lucasReis012@users.noreply.github.com>
2026-04-01 22:59:29 -0300

    adicionei mais alguns metodos, e comecei a implementar no matodo main

--- Alunos.java
+++ Alunos.java
@@ -10,6 +10,7 @@ public class Alunos {//começo alunos
     double tamanho;
 
     public void cadastro (Scanner leitor) {//final Cadastro
+        System.out.println("");
         System.out.println("Nome do Aluno(a)");
         nome.add(leitor.nextLine().trim().toLowerCase());
         while (true) {
@@ -17,7 +18,15 @@ public class Alunos {//começo alunos
                 System.out.println("Qual a nota Do aluno");
                 notas.add(leitor.nextDouble());
                 leitor.nextLine();
-                break;
+                System.out.println("");
+                if(notas.getLast()>10 || notas.getLast()<0){
+                    System.out.println("Erro, você digiu uma nota errada, tem que ser menor que 10 e maior que 0");
+                }else{
+                    tamanho++;
+                    break;
+                }
+
+
             }catch(InputMismatchException e){
                 System.out.println("Erro, você digito uma letra. tem que ser um numero.");
                 leitor.next();
@@ -25,12 +34,16 @@ public class Alunos {//começo alunos
         }
     }//final Cadastro
 
-    public  void mostraNota(int i){//começo mostraNota
-        System.out.println("o nome do aluno é: " + nome.get(i));
-        System.out.println("A nota do(a) " + nome.get(i) +  " é :" + notas.get(i));
+    public  void mostraNota(){//começo mostraNota
+        for(int i = 0; i<tamanho;i++){
+            System.out.println("o nome do aluno é: " + nome.get(i));
+            System.out.println("A nota do(a) " + nome.get(i) +  " é :" + notas.get(i));
+        }
     }//final mostraNota
+
     public void buscaAluno(Scanner leitor){//começo busca aluno
         tamanho = notas.size();
+        System.out.println("");
         System.out.println("Nome do Aluno(a) que deseja buscar?");
         String busca=leitor.nextLine().trim().toLowerCase();
         for(int i =0 ; i<tamanho ; i++){
@@ -42,18 +55,21 @@ public class Alunos {//começo alunos
     }//final busca alunos
     public void atualizaNota(Scanner leitor){//começo atualizaNota
         tamanho = notas.size();
+        System.out.println("");
         System.out.println("De qual aluno deseja mudar a nota?");
         String alunoAtualizado = leitor.nextLine().trim().toLowerCase();
         for (int i =0 ; i<tamanho; i++){
             if (nome.get(i).equals(alunoAtualizado)){
-                System.out.println("Digite o valor.");
+                System.out.println("Digite a nova nota.");
                 notas.set(i , leitor.nextDouble());
+                leitor.nextLine();
                 break;
             }
         }
     }// fim atualizaNota
     public void removeAluno(Scanner leitor){//começo metodo remove
         tamanho = notas.size();
+        System.out.println("");
         System.out.println("Deseja excluir qual aluno?");
         String alunoRemovido = leitor.nextLine().trim().toLowerCase();
             for (int i =0 ; i<tamanho; i++){
@@ -65,6 +81,56 @@ public class Alunos {//começo alunos
                 }
             }
     }//final meotodo remove
+
+    public void ranking (Scanner leitor){//começo ranking
+        tamanho = notas.size();//ver durante a execução, altera para -1
+        double [] auxNotas = new double[(int)tamanho] ;
+        String [] auxAlunos = new String[(int)tamanho];
+        String nomesAux;
+        double notasAux;
+        boolean controle;
+
+        //copiando array
+        for (int i = 0; i<tamanho;i++){
+            auxAlunos[i]=nome.get(i);
+            auxNotas[i]=notas.get(i);
+        }
+
+        //bubble sort
+        for (int i = 0; i<tamanho ; i++){
+            controle = true;
+            for (int j = 0 ; j<(tamanho-1) ; j++ ){
+                if(auxNotas[j]>auxNotas[j+1]){
+                    notasAux = auxNotas[j];
+                    nomesAux = auxAlunos[j];
+
+                    auxNotas[j] = auxNotas[j+1];
+                    auxAlunos[j] = auxAlunos[j+1];
+
+                    auxAlunos[j+1] = nomesAux;
+                    auxNotas[j+1] = notasAux;
+                    controle = false;
+                }
+            }
+            if(controle){
+                break;
+            }
+        }
+
+        //mostrando o ranking
+
+        System.out.println("qual o tamanho do ranking? ");
+        int tamanhoRanking = leitor.nextInt();
+        leitor.nextLine();
+
+        for (int i = 0 ; i<tamanhoRanking ; i++){
+            System.out.println((i+1) + " do Ranking é : " + auxAlunos[i]);
+            System.out.println("A nota é : " + auxNotas[i]);
+
+        }
+
+
+    }//fim ranking
 
 
 }//Final alunos
--- Main.java
+++ Main.java
@@ -1,28 +1,138 @@
 package SistemaDeCadastroComBusca;
+import java.awt.desktop.SystemEventListener;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
-public class Main {
+public class Main {//começo programa
+
+    public  static  int menuInicial(Scanner leitor){//final metodo menuInicial
+        while(true){
+            try{
+                System.out.println("O que deseja fazer?");
+                System.out.println("");
+                System.out.println("[1] - Cadastro aluno.");
+                System.out.println("[2] - Mostrar todos ");
+                System.out.println("[3] - Busca pelo nome ");
+                System.out.println("[4] - Atualizar nota de aluno");
+                System.out.println("[5] - Remover Aluno");
+                System.out.println("[6] - Ranking de alunos.");
+                System.out.println("[7] - Sair.");
+                int escolha = leitor.nextInt();
+                leitor.nextLine();
+                if(escolha <0 || escolha>7){
+                    System.out.println("Erro, Você digitou um numero diferente das opções");
+                }else {
+                    return  escolha;
+                }
+
+
+            } catch (InputMismatchException e) {
+                System.out.println("Erro, incorreto tente novamente!");
+                leitor.nextLine();
+            }
+        }
+
+
+
+    }//final metodo menuInicial
+
+    public static  String controle(Scanner leitor){//começo controle
+        while (true){
+            System.out.println("");
+            System.out.println("Gostaria de continuar? (sim/nao)");
+            String controle = leitor.nextLine().trim().toLowerCase();;;
+            System.out.println("");
+            if(controle.trim().equalsIgnoreCase("sim") || controle.trim().equalsIgnoreCase("nao")){
+                return controle;
+            }else {
+                System.out.println("Erro, você digitou algo diferente, tente novamente");
+                leitor.nextLine();
+            }
+        }
+    }//final metodo controle
     public static void main (String[] args){//metodo main começo
+        //inicialização do Scanner
         Scanner leitor = new Scanner(System.in);
+        //inicialização do objeto alunos.
         Alunos alunos = new Alunos();
+        //menu - inicialização
+        int escolhaUsuario;
+        //controle do laço
+        String controle;
+        //trava
+        boolean trava = false;
 
-//        //cadastro
-//        alunos.cadastro(leitor);
-//        //mostra
-////        alunos.mostraNota(i);
+        do {//começo do-while
+            escolhaUsuario= menuInicial(leitor);
+            if(escolhaUsuario ==7){
+                System.out.println("Finalizando Sistema... Até logo!");
+                break;
+            }
+            switch (escolhaUsuario){//inicio switch
+                case 1:
+                    System.out.println("Quantos alunos gostaria de cadastrar");
+                    int quantidade = leitor.nextInt();
+                    leitor.nextLine();
+                     for(int i = 0; i< quantidade;i++){
+                         alunos.cadastro(leitor);
 
-        for(int i = 0 ; i<3 ; i++){
-            alunos.cadastro(leitor);
+                     }
 
-        }
+                        trava = true;
+                        break;
 
+                case 2:
+                        if(trava){
+                            alunos.mostraNota();
+                            break;
+                        }else{
+                            System.out.println("Realize o cadastro antes.");
+                            break;
+                        }
 
+                case 3 :
+                        if(trava){
+                            alunos.buscaAluno(leitor);
+                            break;
+                        }else{
+                            System.out.println("Realize o cadastro antes.");
+                            break;
+                        }
+                case 4 :
+                    if(trava){
+                        alunos.atualizaNota(leitor);
+                        break;
+                    }else{
+                        System.out.println("Realize o cadastro antes.");
+                        break;
+                    }
 
-        alunos.removeAluno(leitor);
+                case 5  :
+                    if(trava){
+                        alunos.removeAluno(leitor);
+                        break;
+                    }else{
+                        System.out.println("Realize o cadastro antes.");
+                        break;
+                    }
+                case 6  :
+                    if(trava){
+                        alunos.ranking(leitor);
+                        break;
+                    }else{
+                        System.out.println("Realize o cadastro antes.");
+                        break;
+                    }
+                default:
+                    System.out.println("Finalizando Sistema... Até logo!");
+                    break;
+            }//Final switch
 
-        for(int i = 0 ; i<3 ; i++){
-            alunos.mostraNota(i);
-        }
+            controle=controle(leitor);
+
+        }while(controle.equals("sim")); //final do-while
+
 
     }//metodo main final
-}
+}//fim programa
